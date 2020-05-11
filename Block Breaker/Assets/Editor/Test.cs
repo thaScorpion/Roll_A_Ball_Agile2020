a8bdbc503c628197a6c82b2483280ce6d0305752
Elkhalifa Oussama <elkhalifaoussama@gmail.com>
2020-05-11 23:34:12 +0000

    test

---
+++ Block Breaker/Assets/Editor/Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+
+public class Test 
+{
+    [Test]
+    public void CalculatePoints_test(){
+        var GameSession = new GameSession();
+        var destroyedBlocks = 50;
+        Var expectedPoints = 50*83;
+        var points = GameSession.AddToScore();
+
+        Assert.that(points, Is.EqualTo(expectedPoints));
+    }
+}
