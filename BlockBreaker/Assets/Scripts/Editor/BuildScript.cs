99de922be4ea015b55ce9d8d937f9305bfb330e1
thaScorpion <mohamed.aataoui@um5s.net.ma>
2020-06-24 17:28:16 +0200

    copie du fichier log généré par jenkins

---
+++ BlockBreaker/Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using System.Diagnostics;
+#endif
+public class BuildScript
+{
+    public static void BuildGame()
+    {
+        // Get filename.
+        string path = EditorUtility.SaveFolderPanel("build",
+                                        "C:\\Users\aatao\\Desktop\\Blocks_Breaker\\Build",
+                                         "build");
+
+        string[] levels = new string[] { "Assets/Start Menu.unity", "Assets/Level 1.unity",
+                                        "Assets/Level 2.unity", "Assets/Level 3.unity",
+                                        "Assets/Level 4.unity", "Assets/Level 5.unity",
+                                        "Assets/Game Over.unity"};
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // Run the game (Process class from System.Diagnostics).
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + "/BuiltGame.exe";
+        proc.Start();
+    }
+}
---
+++ BlockBreaker/Assets/Scripts/GameSession.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour {
+
+    // config params
+    [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
+    [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] bool isAutoPlayEnabled;
+    [SerializeField] bool isPaused = false;
+    public GameObject pauseMenuUI;
+
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();    
+    }
+
+    // Update is called once per frame
+    void Update () {
+              if(Input.GetKeyDown("p") && !isPaused)
+    {
+       print("p");
+       Time.timeScale = 0;
+       isPaused = true;
+    }
+    else if(Input.GetKeyDown("p") && isPaused)
+    {
+       print("Unpaused");
+       Time.timeScale = 1;
+       isPaused = false;    
+    } 
+	}
+
+    public void AddToScore()
+    {
+        currentScore += pointsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+    public bool IsAutoPlayEnabled()
+    {
+        return isAutoPlayEnabled;
+    }
+}
